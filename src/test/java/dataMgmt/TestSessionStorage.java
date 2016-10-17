82fdaf2eca7fde032faaf19a8414e57cf450bec9
fahslaj <fahslaj@rose-hulman.edu>
2016-10-17 02:48:58 -0400

    Update sessionstorage tests

--- src/test/java/dataMgmt/TestSessionStorage.java
+++ src/test/java/dataMgmt/TestSessionStorage.java
@@ -2,8 +2,10 @@ package dataMgmt;
 
 import org.junit.Assert;
 import org.junit.Test;
+import websocket.models.Project;
 
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 /**
  * Created by fahslaj on 5/9/2016.
@@ -31,5 +33,40 @@ public class TestSessionStorage {
         Assert.assertEquals(null, storage.getAuthenticationToken());
         storage.setAuthenticationToken("Hello");
         Assert.assertEquals("Hello", storage.getAuthenticationToken());
+    }
+
+    @Test
+    public void testUsernameAndPropertyListener() {
+        SessionStorage storage = DataManager.getInstance().getSessionStorage();
+        final int[] access = {0};
+        PropertyChangeListener listener = (event) -> {
+            if (event.getPropertyName().equals(SessionStorage.USERNAME)) {
+                access[0]++;
+            }
+        };
+        storage.addPropertyChangeListener(listener);
+        Assert.assertEquals(null, storage.getUsername());
+        storage.setUsername("austin");
+        Assert.assertEquals("austin", storage.getUsername());
+        Assert.assertEquals(1, access[0]);
+    }
+
+    @Test
+    public void testProjectList() {
+        SessionStorage storage = DataManager.getInstance().getSessionStorage();
+        final int[] access = {0};
+        PropertyChangeListener listener = (event) -> {
+            if (event.getPropertyName().equals(SessionStorage.PROJECT_LIST)) {
+                access[0]++;
+            }
+        };
+        storage.addPropertyChangeListener(listener);
+        Assert.assertEquals(0, storage.getProjects().size());
+        Project project = new Project(0, "hi", null, 0);
+        storage.setProject(project);
+        Assert.assertEquals(1, access[0]);
+        Assert.assertEquals(project, storage.getProjectById(0));
+        storage.setProjects(new ArrayList<>());
+        Assert.assertEquals(2, access[0]);
     }
 }
